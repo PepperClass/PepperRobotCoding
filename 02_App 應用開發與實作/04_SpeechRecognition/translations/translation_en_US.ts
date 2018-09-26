2d81a7f8fcba37e79147d59865220c56d38301fd
GarfieldWang <garfield.wang@perobot.com.tw>
2018-09-26 14:59:57 +0800

    SpeechRecognition

---
+++ 02_App 應用開發與實作/04_SpeechRecognition/translations/translation_en_US.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>你好</source>
+            <comment>Text</comment>
+            <translation type="unfinished">你好</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>我是 Pepper</source>
+            <comment>Text</comment>
+            <translation type="unfinished">我是 Pepper</translation>
+        </message>
+    </context>
+</TS>
