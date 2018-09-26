4e7a34b028d5a5172ea8f70ae661c03df70a137c
GarfieldWang <garfield.wang@perobot.com.tw>
2018-09-26 15:58:52 +0800

    Pepper Interactive

---
+++ 02_App 應用開發與實作/08_Pepper_Interactive/translations/translation_en_US.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>你好，有什麼可以為您服務</source>
+            <comment>Text</comment>
+            <translation type="unfinished">你好，有什麼可以為您服務</translation>
+        </message>
+    </context>
+</TS>
---
+++ 02_App 應用開發與實作/08_Pepper_Interactive/translations/translation_zh_CN.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>你好，有什麼可以為您服務</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+</TS>
