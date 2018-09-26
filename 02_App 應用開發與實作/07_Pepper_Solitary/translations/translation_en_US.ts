ff8c676af3aadc1e615054bb6df10e215d1b0573
GarfieldWang <garfield.wang@perobot.com.tw>
2018-09-26 15:56:41 +0800

    Pepper Solitary mode

---
+++ 02_App 應用開發與實作/07_Pepper_Solitary/translations/translation_en_US.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>怎麼沒有人，Pepper 好無聊</source>
+            <comment>Text</comment>
+            <translation type="unfinished">怎麼沒有人，Pepper 好無聊</translation>
+        </message>
+    </context>
+</TS>
---
+++ 02_App 應用開發與實作/07_Pepper_Solitary/translations/translation_zh_CN.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>怎麼沒有人，Pepper 好無聊</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+</TS>
