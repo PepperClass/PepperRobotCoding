d8628f43693d408cb34fd9048fbc0700e4bd489d
GarfieldWang <garfield.wang@perobot.com.tw>
2018-09-26 14:14:01 +0800

    set Language

--- 02_App 應用開發與實作/02_SetLanguage/translations/translation_en_US.ts
+++ 02_App 應用開發與實作/02_SetLanguage/translations/translation_en_US.ts
@@ -4,9 +4,28 @@
     <context>
         <name>behavior_1/behavior.xar:/Say</name>
         <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Hello</source>
             <comment>Text</comment>
             <translation>Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>你好</source>
+            <comment>Text</comment>
+            <translation type="unfinished">你好</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>こんにちは</source>
+            <comment>Text</comment>
+            <translation type="unfinished">こんにちは</translation>
         </message>
     </context>
 </TS>
