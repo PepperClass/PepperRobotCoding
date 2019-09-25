6a8494886c9e9848b65ac4512a85734564d9c992
Batwang <garfiledwang@gmail.com>
2019-09-25 11:11:08 +0800

    TabletButtonToPython
    Trigger Event to Python

---
+++ 03_Pepper API 串接示範與實作/11_TabletButtonSample/translations/translation_en_US.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation>Hello</translation>
+        </message>
+    </context>
+</TS>
